ab40448dc164952600b0d0da06680582c2178684
Kryto627 <kryto627@gmail.com>
2015-12-05 23:43:50 -0500

    Change render engine

--- src/com/kryto/scrap/GLSprite.java
+++ src/com/kryto/scrap/GLSprite.java
@@ -27,66 +27,17 @@ public class GLSprite {
 	}
 	
 	public void render(float x, float y) {
-		render(x, y, texture.getWidth(), texture.getHeight());
+		render(x, y, texture.getTextureWidth(), texture.getTextureHeight());
 	}
 	
-	public void render(float x, float y, float w, float h) {
+	public void render(float x, float y, float width, float height) {
 		
-		texture.bind();		
-		
-		GL11.glBegin(GL11.GL_QUADS);
-		
-			GL11.glTexCoord2f(0, 0);
-			GL11.glVertex2f(x, y);
-			
-			GL11.glTexCoord2f(1, 0);
-			GL11.glVertex2f(x + w, y);
-			
-			GL11.glTexCoord2f(1, 1);
-			GL11.glVertex2f(x + w, y + h);
-			
-			GL11.glTexCoord2f(0, 1);
-			GL11.glVertex2f(x, y + h);
-		
-		GL11.glEnd();
-		
-		GL11.glLoadIdentity();
+		RenderUtil.renderTexture(texture, x, y, width, height);
 	}
 	
 	public void renderSubTile(float renderX, float renderY, float renderWidth, float renderHeight, int tileX, int tileY, int tileWidth, int tileHeight) {
 		
-		texture.bind();
-		
-		float pixelX = 1.0F / texture.getTextureWidth();
-		float pixelY = 1.0F / texture.getTextureHeight();
-		
-		float beginX = pixelX * tileX;
-		float beginY = pixelY * tileY;
-		
-		float endX = (pixelX * tileWidth) + beginX;
-		float endY = (pixelY * tileHeight) + beginY;
-		
-		GL11.glBegin(GL11.GL_QUADS);
-			
-			GL11.glTexCoord2f(beginX, beginY);
-			
-			GL11.glVertex2f(renderX, renderY);
-			
-			GL11.glTexCoord2f(endX, beginY);
-			
-			GL11.glVertex2f(renderX + renderWidth, renderY);
-			
-			GL11.glTexCoord2f(endX, endY);
-			
-			GL11.glVertex2f(renderX + renderWidth, renderY + renderHeight);
-			
-			GL11.glTexCoord2f(beginX, endY);
-			
-			GL11.glVertex2f(renderX, renderY + renderHeight);
-		
-		GL11.glEnd();
-		
-		GL11.glLoadIdentity();
+		RenderUtil.renderTexture(texture, renderX, renderY, renderWidth, renderHeight, tileX, tileY, tileWidth, tileHeight);
 	}
 	
 	public void renderSubIndex(float x, float y, float w, float h, int i, int tileWidth, int tileHeight) {
@@ -100,11 +51,11 @@ public class GLSprite {
 		renderSubTile(x, y, w, h, tileX, tileY, tileWidth, tileHeight);
 	}
 	
-	public Texture getTexture() {
-		return texture;
-	}
-	
 	public GLSubSprite getSubSprite(int tileX, int tileY, int tileWidth, int tileHeight) {
 		return new GLSubSprite(this, tileX, tileY, tileWidth, tileHeight);
+	}
+	
+	public Texture getTexture() {
+		return texture;
 	}
 }
--- src/com/kryto/scrap/RenderUtil.java
+++ src/com/kryto/scrap/RenderUtil.java
@@ -2,26 +2,82 @@ package com.kryto.scrap;
 
 import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
 
 public class RenderUtil {
 
-	public static void renderRect(float x, float y, float width, float height, Color color) {
-			
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
+	public static void renderTexture(Texture texture, float x, float y, float width, float height) {
+
+		texture.bind();
+
+		GL11.glBegin(GL11.GL_QUADS);
+
+		GL11.glTexCoord2f(0, 0);
+		GL11.glVertex2f(x, y);
+
+		GL11.glTexCoord2f(1, 0);
+		GL11.glVertex2f(x + width, y);
+
+		GL11.glTexCoord2f(1, 1);
+		GL11.glVertex2f(x + width, y + height);
+
+		GL11.glTexCoord2f(0, 1);
+		GL11.glVertex2f(x, y + height);
+
+		GL11.glEnd();
+
+		GL11.glLoadIdentity();
+	}
+
+	public static void renderTexture(Texture texture, float x, float y, float width, float height, int tileX, int tileY, int tileWidth, int tileHeight) {
+
+		texture.bind();
 		
-		color.bind();
+		float pixelX = 1.0F / texture.getTextureWidth();
+		float pixelY = 1.0F / texture.getTextureHeight();
+
+		float beginX = pixelX * tileX;
+		float beginY = pixelY * tileY;
+		
+		float endX = (pixelX * tileWidth) + beginX;
+		float endY = (pixelY * tileHeight) + beginY;
 		
 		GL11.glBegin(GL11.GL_QUADS);
-		
-			GL11.glVertex2f(x, y);
-			GL11.glVertex2f(x + width, y);
-			GL11.glVertex2f(x + width, y + height);
-			GL11.glVertex2f(x, y + height);
-		
+
+		GL11.glTexCoord2f(beginX, beginY);
+		GL11.glVertex2f(x, y);
+
+		GL11.glTexCoord2f(endX, beginY);
+		GL11.glVertex2f(x + width, y);
+
+		GL11.glTexCoord2f(endX, endY);
+		GL11.glVertex2f(x + width, y + height);
+
+		GL11.glTexCoord2f(beginX, endY);
+		GL11.glVertex2f(x, y + height);
+
 		GL11.glEnd();
-		
+
+		GL11.glLoadIdentity();
+	}
+
+	public static void renderRect(float x, float y, float width, float height, Color color) {
+
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+		color.bind();
+
+		GL11.glBegin(GL11.GL_QUADS);
+
+		GL11.glVertex2f(x, y);
+		GL11.glVertex2f(x + width, y);
+		GL11.glVertex2f(x + width, y + height);
+		GL11.glVertex2f(x, y + height);
+
+		GL11.glEnd();
+
 		Color.white.bind();
-		
+
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 	}
 }
